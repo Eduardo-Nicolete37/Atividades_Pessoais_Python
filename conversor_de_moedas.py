92dcab7350e5a0690772023d9c50265451a0abb1
Eduardo-Nicolete37 <eduardonicolete37@gmail.com>
2026-05-09 15:56:47 -0300

    09.05.26 - Eduardo Nicolete - Finalização do código de conversão de moedas

--- conversor_de_moedas.py
+++ conversor_de_moedas.py
@@ -1,6 +1,81 @@
-print("|Conversor de Moedas|")
-print("|Qual moeda você quer converter?|")
-print("|1 - Real(R$)|")
-print("|2 - Dolár($)|")
-print("|3 - Euro(R$)|")
-print("|4 - Real(R$)|")
+import os
+import sys
+import requests
+os.system('cls')
+puller = ""
+
+
+print("|         Conversor de Moedas          |")
+print("| De qual moeda você deseja converter? |")
+print("| 1 - Real(R$)                         |")
+print("| 2 - Dolár($)                         |")
+print("| 3 - Euro(€)                          |")
+print("| 4 - Libra(£)                         |")
+in_money = int(input("| Escolha: "))
+
+os.system('cls')
+if  in_money == 1:
+    puller = "BRL-"
+    first = "Real"
+elif in_money == 2:
+    puller = "USD-"
+    first = "Dolár"
+elif in_money == 3:
+    puller = "EUR-"
+    first = "Euro"
+elif in_money == 4:
+    puller = "GBP-"
+    first = "Libra"
+else:
+    print("MOEDA FORA DA LISTA! TENTE NOVAMENTE")
+    sys.exit()
+    
+
+print("| Para qual moeda você deseja converter? |")
+print("| 1 - Real(R$)                           |")
+print("| 2 - Dolár($)                           |")
+print("| 3 - Euro(€)                            |")
+print("| 4 - Libra(£)                           |")
+out_money = int(input("| Escolha: "))
+
+os.system('cls')
+if  out_money == 1:
+    puller += "BRL"
+    last = "Real"
+    final = "R$"
+elif out_money == 2:
+    puller += "USD"
+    last = "Dolár"
+    final = "$"
+elif out_money == 3:
+    puller += "EUR"
+    last = "Euro"
+    final = "€"
+elif out_money == 4:
+    puller += "GBP"
+    last = "Libra"
+    final = "£"
+else:
+    print("MOEDA FORA DA LISTA! TENTE NOVAMENTE")
+    sys.exit()
+
+os.system('cls')
+
+if in_money == out_money:
+    print("MOEDAS IGUAIS! TENTE NOVAMENTE ESCOLHENDO OUTRAS OPÇÕES")
+    sys.exit()
+    
+
+
+count = float(input("Digite o quanto você quer converter: "))
+
+request = requests.get(f'https://economia.awesomeapi.com.br/json/last/{puller}')
+dados = request.json()
+
+chave = puller.replace("-", "")
+valor_moeda = float(dados[chave]["bid"])
+valor_moeda_final = count * valor_moeda
+os.system('cls')
+
+print(f"A conversão do(a) {first} para o(a) {last} é: {final}{valor_moeda:.2f}")
+print(f"Multiplicado pelo {count}, é igual á: {final}{valor_moeda_final}")
